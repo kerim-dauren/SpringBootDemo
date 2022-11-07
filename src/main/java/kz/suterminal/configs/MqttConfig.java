16bc3727ddb5193e2f200d7ebb56b2fea48d5ad9
Dauren Kerimbekov <kerim.dauren@gmail.com>
2022-11-07 21:39:37 +0600

    добавил mqtt subscriber

--- src/main/java/kz/suterminal/configs/MqttConfig.java
+++ src/main/java/kz/suterminal/configs/MqttConfig.java
@@ -2,40 +2,86 @@ package kz.suterminal.configs;
 
 import lombok.Data;
 import lombok.SneakyThrows;
-import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
 
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "mqtt")
 public class MqttConfig {
-
-    private static final Logger logger = LoggerFactory.getLogger(MqttConfig.class);
     private String url;
     private String username;
     private String password;
     private int connectionTimeoutInSeconds;
 
 
-    @SneakyThrows
-    @Bean
-    public IMqttAsyncClient createClient() {
+    public MqttConnectOptions mqttConnectOptions() {
         var options = new MqttConnectOptions();
         options.setAutomaticReconnect(true);
         options.setCleanSession(true);
         options.setConnectionTimeout(connectionTimeoutInSeconds);
         options.setUserName(username);
         options.setPassword(password.toCharArray());
+        return options;
+    }
 
+    @SneakyThrows
+    @Bean
+    public IMqttAsyncClient createClient() {
         var client = new MqttAsyncClient(url, MqttAsyncClient.generateClientId());
         if (!client.isConnected()) {
-            client.connect(options);
+            client.connect(mqttConnectOptions());
         }
+
         return client;
+    }
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageProducer inbound() {
+        var factory = new DefaultMqttPahoClientFactory();
+        factory.setConnectionOptions(mqttConnectOptions());
+
+        var adapter = new MqttPahoMessageDrivenChannelAdapter(url, MqttClient.generateClientId(), factory, "device/#");
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return new MessageHandler() {
+
+            @SneakyThrows
+            @Override
+            public void handleMessage(Message<?> message) {
+                System.out.println(message.getPayload());
+            }
+
+        };
     }
 
 }
---
+++ src/main/java/kz/suterminal/models/entities/DeviceEvent.java
@@ -0,0 +1,8 @@
+package kz.suterminal.models.entities;
+
+import lombok.Data;
+
+@Data
+public class DeviceEvent {
+    private String imei;
+}
